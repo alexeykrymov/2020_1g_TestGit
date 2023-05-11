a69992775c64a638ab9f6496760821620c2e4ac5
Alexey Krymov <alexeykrymov@gmail.com>
2023-05-11 16:48:01 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_1g_TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
